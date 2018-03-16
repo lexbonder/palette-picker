3bac81a08a26f56f034b34f16a01550a26fd08a4
Alex Bonder <lexbonder@gmail.com>
2018-03-16 10:47:32 -0600

    Add production environment

--- knexfile.js
+++ knexfile.js
@@ -24,38 +24,15 @@ module.exports = {
     seeds: {
       directory: './db/seeds/test'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
